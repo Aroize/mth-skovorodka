5a98f300a276d5cd2444837fa0737623b0b9570e
Ilia <gipermonk@bk.ru>
2021-03-27 21:16:42 +0300

    impl auth reg

--- serv/app.py
+++ serv/app.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask import Flask, request, jsonify, Response
 from database import *
 app = Flask(__name__)
@@ -22,6 +21,40 @@ def auth():
             response["result"] = user.json()
             code = 200
     return jsonify(response), code
+
+
+@app.route('/user.register', methods=['POST', 'GET'])
+def register():
+    email = request.args.get('email')
+
+    user = db_helper.user_by_email(email)
+
+    response = {}
+    code = 0
+    if user != None:
+        response['msg'] = "User already exists"
+        code = 403
+    else:
+        pwd = request.args.get('pwd')
+        if len(pwd) < 8:
+            response['msg'] = "Password must be at least 8 symbols"
+            code = 403
+        else:
+            name = request.args.get('name')
+            surname = request.args.get('surname')
+            age = request.args.get('age')
+            user = db_helper.insert_user(email, pwd, name, surname, age)
+            if user != None:
+                response['result'] = user.json()
+                code = 200
+            else:
+                response['msg'] = "Internall Error"
+                code = 503
+    return jsonify(response), code
+
+
+    
+
 
 
 if __name__ == '__main__':
--- serv/database.py
+++ serv/database.py
@@ -1,55 +1,74 @@
 import sqlite3
 
+class User:
+
+	def __init__(self, id, email, pwd, name, surname, age):
+		self.id = id
+		self.email = email
+		self.pwd = pwd
+		self.name = name
+		self.surname = surname
+		self.age = age
+
+	def json(self):
+		return {
+			'id': self.id,
+			'email': self.email,
+			'name': self.name,
+			'surname': self.surname,
+			'age': self.age
+		}
+
 class DBOpenHelper:
 
 	def __init__(self):
-		self.db = sqlite3.connect("skovorodka.db")
+		self.db = sqlite3.connect("skovorodka2.db")
 		cursor = self.db.cursor()
 		try:
 			cursor.execute("""
 				CREATE TABLE IF NOT EXISTS users
 				( 
-					id int AUTO_INCREMENT PRIMARY KEY,
-					email varchar(50) NOT NULL,
-					pwd varchar(50) NOT NULL,
-					name varchar(30) NOT NULL,
-					surname varchar(30) NOT NULL,
-					age int NOT NULL
+					id INTEGER PRIMARY KEY AUTOINCREMENT,
+					email STRING NOT NULL,
+					pwd STRING NOT NULL,
+					name STRING NOT NULL,
+					surname STRING NOT NULL,
+					age INTEGER NOT NULL
 				)
 				"""
 			)
 			cursor.execute("""
 				CREATE TABLE IF NOT EXISTS papers
 				(
-					id int AUTO_INCREMENT PRIMARY KEY,
-					path varchar(200) NOT NULL
+					id INTEGER PRIMARY KEY AUTOINCREMENT,
+					path STRING NOT NULL
 				)
 				"""
 			)
 			cursor.execute("""
 				CREATE TABLE IF NOT EXISTS favs
 				(
-					u_id int NOT NULL,
-					p_id int NOT NULL
+					u_id INTEGER NOT NULL,
+					p_id INTEGER NOT NULL
 				)
 				"""
 			)
 			cursor.execute("""
 				CREATE TABLE IF NOT EXISTS cosine
 				(
-					first int NOT NULL,
-					second int NOT NULL,
-					cosine float NOT NULL
+					first INTEGER NOT NULL,
+					second INTEGER NOT NULL,
+					cosine FLOAT NOT NULL
 				)
 				"""
 			)
 			cursor.execute("""
 				CREATE TABLE IF NOT EXISTS stats
 				(
-					u_id int NOT NULL,
-					p_id int NOT NULL,
+					u_id INTEGER NOT NULL,
+					p_id INTEGER NOT NULL,
 					ts long NOT NULL,
-					type varchar(20)
+					type STRING
 				)
 				"""
 			)
@@ -67,12 +86,28 @@ class DBOpenHelper:
 			result = cursor.execute(sql, (email,))
 			user = result.fetchone()
 			if user != None:
-				pass
+				return User(*user)
 			return None
 		except:
 			return None
 		finally:
 			cursor.close()
+
+	def insert_user(self, email, pwd, name, surname, age):
+		cursor = self.db.cursor()
+		try:
+			sql = "INSERT INTO users (email, pwd, name, surname, age) VALUES {}"
+			args = (email, pwd, name, surname, age)
+			cursor.execute(sql.format(args))
+			self.db.commit()
+			id = cursor.lastrowid
+			return User(id, email, pwd, name, surname, age)	
+		except:
+			return None
+		finally:
+			cursor.close()
+		
+			
 
 
 db_helper = DBOpenHelper()
