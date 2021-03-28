abbaefdfc2e49a194cc6e36ec05ef30a038ab2f8
Egazaga <zg2000@yandex.ru>
2021-03-28 03:38:25 +0300

    Recs

--- ml/ml_func.py
+++ ml/ml_func.py
@@ -1,12 +1,10 @@
 from glob import glob
+from random import random, sample
+
 import numpy as np
 
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
-
-
-def get_starting_recommendations(interests, already_read, age):
-    pass
 
 
 def get_big_themes():
@@ -29,3 +27,36 @@ def _count_and_save_simm_matrix():
 
 def get_simm_matrix():
     return np.load("simm_matrix.npy")
+
+
+def get_starting_recommendations(interests, already_read, diff):
+    recs = []
+    articles_from_small_interests = []  # use diff +-3, if >10 resulting articles
+    if len(articles_from_small_interests) >= 10:  # sort by diff
+        recs.append(sample(articles_from_small_interests, 10))
+    else:
+        recs.append(sample(articles_from_small_interests, len(articles_from_small_interests)))
+
+    articles_from_big_interests = []  # use diff +-3, if >10 resulting articles
+    if len(articles_from_big_interests) >= 10:  # sort by diff
+        recs.append(sample(articles_from_big_interests, 10))
+    else:
+        recs.append(sample(articles_from_big_interests, len(articles_from_big_interests)))
+
+    all_articles = []  # anarchy
+    if len(all_articles) >= 10:  # sort by diff
+        recs.append(sample(all_articles, 10))
+    else:
+        recs.append(sample(all_articles, len(all_articles)))
+
+    return recs
+
+
+def get_simm_recommendations(id, diff, already_read):
+    simm = get_simm_matrix()[id]
+    if np.isclose(np.sum(simm), 0):
+        raise ValueError("No such id")
+    simm[already_read] = 0.0
+    recs = (-simm).argsort()[:5]
+    # TODO sort by diff
+    return recs
